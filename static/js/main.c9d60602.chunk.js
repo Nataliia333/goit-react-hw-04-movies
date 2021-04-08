7b3eb7a887b01d67829aef9f8b00211432adccf0
Nataliia333 <n.o.prykhodko@gmail.com>
2021-04-08 23:19:17 +0300

    Updates

---
+++ static/js/main.c9d60602.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-04-movies"]=this["webpackJsonpgoit-react-hw-04-movies"]||[]).push([[0],{10:function(t,e,n){"use strict";n.r(e);var c=n(0),o=n.n(c),i=n(3),r=n.n(i);n(8);var s=function(){},a=n(2);r.a.render(Object(a.jsx)(o.a.StrictMode,{children:Object(a.jsx)(s,{})}),document.getElementById("root"))},8:function(t,e,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.c9d60602.chunk.js.map
