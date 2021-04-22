2704046e98fc87f26f1b58d6b91a7902f74654eb
Nataliia333 <n.o.prykhodko@gmail.com>
2021-04-22 22:27:14 +0300

    маршрутизация

--- src/App.js
+++ src/App.js
@@ -1,3 +1,30 @@
-function App() {}
+import React from 'react';
+import { Route, NavLink, Switch } from 'react-router-dom';
+import HomePage from './components/views/HomePage/HomePage';
+import MoviesPage from './components/views/MoviesPage/MoviesPage';
+import MovieDetailsPage from './components/views/MovieDetailsPage/MovieDetailsPage';
+import NotFoundPage from './components/views/NotFoundPage/NotFoundPage';
+const App = () => (
+  <>
+    <ul>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Movies</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">MovieDetails</NavLink>
+      </li>
+    </ul>
+
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/movies" component={MoviesPage} />
+      <Route path="/movies/:movieId" component={MovieDetailsPage} />
+      <Route component={NotFoundPage} />
+    </Switch>
+  </>
+);
 
 export default App;
---
+++ src/components/views/MoviesPage/MoviesPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MoviesPage = () => {
+  return <h1>Это страница поиска фильмов по ключевому слову</h1>;
+};
+
+export default MoviesPage;
